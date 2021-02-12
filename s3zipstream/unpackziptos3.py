fe6e18f34291b3e246c21830f23f6a2add622a5f
Gabriel Sargeant <gabe.sargeant@gmail.com>
2021-02-12 23:31:22 +1100

    edits, doco update

--- s3zipstream/unpackziptos3.py
+++ s3zipstream/unpackziptos3.py
@@ -13,9 +13,6 @@ def lambda_handler(event, context):
     try:
         
         logger.info('Event: %s', event)
-        logger.info(event['src'])
-        s3zipsource = event['src'] 
-        dests3 = event['dst']
         
         s3_resource = boto3.resource('s3')
         zip_object = s3_resource.Object(bucket_name=event['src'], key=event['zip'])
@@ -33,7 +30,7 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.error(e)
         traceback.print_exc()
-        raise Exception('Error occured during unpackziptoS3')
+        raise Exception('Error occurred during unpackziptoS3')
     
     return {
         "statusCode": HTTPStatus.OK.value
